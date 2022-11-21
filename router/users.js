0be55d63b82bfb8b661787ed659b1db705290487
tfnyyy23 <fadillahtifany@gmail.com>
2022-11-21 07:27:57 +0700

    modul 1-3 expressjs

---
+++ router/users.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+const usercontroller = require('../controllers/user') 
+
+router.get('/users', usercontroller.index)
+
+router.get('/users/:id', usercontroller.show)
+
+  router.post('/user', usercontroller.store)
+
+  router.put('/user/:id', usercontroller.update)
+
+  router.delete('/user/:id', usercontroller.delete)
+
+  module.exports = router
